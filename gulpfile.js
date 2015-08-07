74c76c92c50bc09a59fbcecc5149eb695cb4273e
arminhammer <arminhammer@gmail.com>
2015-08-06 23:16:35 -0400

    Changing project to a scraping npm library. Initial commit of the lib.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,75 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
-  livereload = require('gulp-livereload');
+'use strict';
+var path = require('path');
+var gulp = require('gulp');
+var excludeGitignore = require('gulp-exclude-gitignore');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var istanbul = require('gulp-istanbul');
+var nsp = require('gulp-nsp');
+var plumber = require('gulp-plumber');
+var coveralls = require('gulp-coveralls');
+var babel = require('gulp-babel');
 
+// Initialize the babel transpiler so ES2015 files gets compiled
+// when they're loaded
+require('babel-core/register');
 
-gulp.task('develop', function () {
-  livereload.listen();
-  nodemon({
-    script: 'bin/www',
-    ext: 'js jade coffee',
-  }).on('restart', function () {
-    setTimeout(function () {
-      livereload.changed(__dirname);
-    }, 500);
-  });
+var handleErr = function (err) {
+  console.log(err.message);
+  process.exit(1);
+};
+
+gulp.task('static', function () {
+  return gulp.src('**/*.js')
+    .pipe(excludeGitignore())
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .pipe(jscs())
+    .on('error', handleErr);
 });
 
-gulp.task('default', [
-  'develop'
-]);
+gulp.task('nsp', function (cb) {
+  nsp('package.json', cb);
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src('lib/**/*.js')    .pipe(babel())
+    
+    .pipe(istanbul({includeUntested: true}))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function (cb) {
+  var mochaErr;
+
+  gulp.src('test/**/*.js')
+    .pipe(plumber())
+    .pipe(mocha({reporter: 'spec'}))
+    .on('error', function (err) {
+      mochaErr = err;
+    })
+    .pipe(istanbul.writeReports())
+    .on('end', function () {
+      cb(mochaErr);
+    });
+});
+
+gulp.task('coveralls', ['test'], function () {
+  if (!process.env.CI) {
+    return;
+  }
+
+  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe(coveralls());
+});
+
+gulp.task('babel', function () {
+  return gulp.src('lib/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('default', ['static', 'test', 'coveralls']);
