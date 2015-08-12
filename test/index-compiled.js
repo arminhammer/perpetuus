776209ef4848422154d5ce884aceb52396718038
arminhammer <arminhammer@gmail.com>
2015-08-11 21:49:26 -0400

    Tests now using done() so async is tested properly.

--- test/index-compiled.js
+++ test/index-compiled.js
@@ -18,33 +18,70 @@ var invalidTemplate = {
   orl: 'http://google.com'
 };
 
-var busyTemplate = {
-  url: 'http://google.com'
+var googleTemplate = {
+  url: 'http://google.com',
+  input: [{
+    element: '#lst-ib',
+    val: 'tigers'
+  }],
+  follow: {
+    method: {
+      click: 'button.lsb'
+    },
+    val: {
+      links: '.g > a[href]'
+    }
+  }
 };
 
 describe('rubbertiger', function () {
-  it('should use promises', function () {
+  it('should use promises', function (done) {
 
     _lib2['default'].scrape(simpleTemplate).then(function (response) {
 
       console.log(response);
       (0, _expectJs2['default'])(response).to.eql('Good.');
+      done();
     })['catch'](function (error) {
       console.log('Error: ' + error);
       (0, _expectJs2['default'])(error).to.be(undefined);
+      done();
     });
   });
 });
 
 describe('rubbertiger', function () {
-  it('should validate the schema', function () {
+  it('should validate the schema', function (done) {
 
     _lib2['default'].scrape(invalidTemplate).then(function (response) {
 
       console.log(response);
+      done();
     })['catch'](function (error) {
       console.log('Error: ' + error);
       (0, _expectJs2['default'])(error).to.be(false);
+      done();
+    });
+  });
+});
+
+describe('rubbertiger', function () {
+  it('should input a search and return links from google', function (done) {
+
+    var expected = {
+      links: ['http://www.google.com', 'http://www.google.com']
+    };
+
+    _lib2['default'].scrape(googleTemplate).then(function (response) {
+
+      console.log('Response from google scrape:');
+      console.log(response);
+      (0, _expectJs2['default'])(response).to.be(expected);
+      done();
+    })['catch'](function (error) {
+      console.log('Error: ' + error);
+      (0, _expectJs2['default'])(error).to.be(undefined);
+      done();
     });
   });
 });
--- test/index.js
+++ test/index.js
@@ -10,13 +10,27 @@ var invalidTemplate = {
   orl: 'http://google.com'
 };
 
-var busyTemplate = {
-  url: 'http://google.com'
+var googleTemplate = {
+  url: 'http://google.com',
+  input: [
+    {
+      element: '#lst-ib',
+      val: 'tigers'
+    }
+  ],
+  follow: {
+    method: {
+      click: 'button.lsb'
+    },
+    val: {
+      links: '.g > a[href]'
+    }
+  }
 };
 
 
 describe('rubbertiger', function () {
-  it('should use promises', function () {
+  it('should use promises', function (done) {
 
     rubbertiger
       .scrape(simpleTemplate)
@@ -24,29 +38,59 @@ describe('rubbertiger', function () {
 
         console.log(response);
         expect(response).to.eql('Good.');
-
+        done();
       })
       .catch(function(error) {
         console.log('Error: ' + error);
         expect(error).to.be(undefined);
+        done();
       });
 
   });
 });
 
 describe('rubbertiger', function () {
-  it('should validate the schema', function () {
+  it('should validate the schema', function (done) {
 
     rubbertiger
       .scrape(invalidTemplate)
       .then(function(response) {
 
         console.log(response);
-
+        done();
       })
       .catch(function(error) {
         console.log('Error: ' + error);
         expect(error).to.be(false);
+        done();
+      });
+
+  });
+});
+
+describe('rubbertiger', function () {
+  it('should input a search and return links from google', function (done) {
+
+    var expected = {
+      links: [
+        'http://www.google.com',
+        'http://www.google.com'
+      ]
+    };
+
+    rubbertiger
+      .scrape(googleTemplate)
+      .then(function(response) {
+
+        console.log('Response from google scrape:');
+        console.log(response);
+        expect(response).to.be(expected);
+        done();
+      })
+      .catch(function(error) {
+        console.log('Error: ' + error);
+        expect(error).to.be(undefined);
+        done();
       });
 
   });
