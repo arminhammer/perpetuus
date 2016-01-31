7568eae858e24eb033483cda8f603e74d96505f2
arminhammer <arminhammer@gmail.com>
2016-01-31 10:22:33 -0500

    Initial commit with one test working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,32 @@
 'use strict';
 var path = require('path');
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
-//var babel = require('gulp-babel');
-
-// Initialize the babel transpiler so ES2015 files gets compiled
-// when they're loaded
-//require('babel-core/register');
-
-var handleErr = function (err) {
-  console.log(err.message);
-  process.exit(1);
-};
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jscs())
-    .on('error', handleErr);
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('nsp', function (cb) {
-  nsp('package.json', cb);
+  nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
   return gulp.src('lib/**/*.js')
-    //.pipe(babel())
-
-    .pipe(istanbul({includeUntested: true}))
+    .pipe(excludeGitignore())
+    .pipe(istanbul({
+      includeUntested: true
+    }))
     .pipe(istanbul.hookRequire());
 });
 
@@ -57,6 +45,10 @@ gulp.task('test', ['pre-test'], function (cb) {
     });
 });
 
+gulp.task('watch', function () {
+  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+});
+
 gulp.task('coveralls', ['test'], function () {
   if (!process.env.CI) {
     return;
@@ -65,13 +57,6 @@ gulp.task('coveralls', ['test'], function () {
   return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
     .pipe(coveralls());
 });
-
-
-//gulp.task('babel', function () {
-  //return gulp.src('lib/**/*.js')
-    //.pipe(babel())
-    //.pipe(gulp.dest('dist'));
-//});
 
 gulp.task('prepublish', ['nsp']);
 gulp.task('default', ['static', 'test', 'coveralls']);
--- test/index.js
+++ test/index.js
@@ -1,97 +1,10 @@
 'use strict';
-var expect = require('expect.js');
+
+var assert = require('assert');
 var rubbertiger = require('../lib');
 
-var simpleTemplate = {
-  url: 'http://www.google.com/'
-};
-
-var invalidTemplate = {
-  orl: 'http://www.google.com/'
-};
-
-var googleTemplate = {
-  'url': 'http://www.google.com/',
-  'input': [
-    {
-      'element': '#lst-ib',
-      'val': 'tigers'
-    }
-  ],
-  'follow': {
-    'method': {
-      'click': 'button.lsb'
-    },
-    'val': {
-      'links': '.g > a[href]'
-    }
-  }
-};
-
-
 describe('rubbertiger', function () {
-  it('should use promises', function (done) {
-
-    rubbertiger
-      .scrape(simpleTemplate)
-      .then(function(response) {
-
-        console.log(response);
-        expect(response).to.eql('Good.');
-        done();
-      })
-      .catch(function(error) {
-        console.log('Error: ' + error);
-        expect(error).to.be(undefined);
-        done();
-      });
-
-  });
-});
-
-describe('rubbertiger', function () {
-  it('should validate the schema', function (done) {
-
-    rubbertiger
-      .scrape(invalidTemplate)
-      .then(function(response) {
-
-        console.log(response);
-        done();
-      })
-      .catch(function(error) {
-        console.log('Error: ' + error);
-        expect(error).to.be(false);
-        done();
-      });
-
-  });
-});
-
-describe('rubbertiger', function () {
-  it('should input a search and return links from google', function (done) {
-
-    var expected = {
-      links: [
-        'http://www.google.com',
-        'http://www.google.com'
-      ]
-    };
-
-    rubbertiger
-      .scrape(googleTemplate)
-      .then(function(response) {
-
-        console.log('Response from google scrape:');
-        console.log(response);
-        expect(response).to.be(expected);
-        done();
-      })
-      .catch(function(error) {
-        console.log('Error: ' + error);
-        expect(error).to.be(undefined);
-        done();
-      });
-
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
